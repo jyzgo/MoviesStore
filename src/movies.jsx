b15b8a642519f3ed8adc0cdbb314b74f3b8e4c54
jyzHome <jyzgo@163.com>
2018-12-31 23:54:18 +1100

    add left list group

--- src/movies.jsx
+++ src/movies.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import ListGroup from "./components/common/listgroup";
 import "./App.css";
 import { getMovies, removeMovie, onLikePressed } from "./services/services";
+import { getGenere } from "./services/fakeGenreService";
 import MovieRow from "./components/MovieRow";
 import Pagination from "./components/common/pagination";
 import { paginate } from "./utils/paginate";
@@ -11,11 +12,25 @@ class Movies extends Component {
     super(props);
   }
 
+  state = {
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+    selectedGenere: "Action"
+  };
   onDeletePress = movie => {
     removeMovie(movie);
     this.setState({ movies: getMovies() });
   };
 
+  componentDidMount() {
+    this.setState({ movies: getMovies(), genres: getGenere() });
+  }
+
+  handleGenereSelect = genre => {
+    this.setState({ selectedGenere: genre });
+  };
   onLikePress = movie => {
     onLikePressed(movie);
     this.setState({ movies: getMovies() });
@@ -24,45 +39,59 @@ class Movies extends Component {
   handlePageChange = page => {
     this.setState({ currentPage: page });
   };
-  state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      selectedGenere,
+      pageSize,
+      currentPage,
+      genres,
+      movies: allMovies
+    } = this.state;
     const movies = paginate(allMovies, currentPage, pageSize);
     return (
-      <React.Fragment>
-        <h2>This is movies</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map(movie => {
-              return (
-                <MovieRow
-                  key={movie._id}
-                  movie={movie}
-                  onDeletePress={this.onDeletePress}
-                  onLikeClick={this.onLikePress}
-                />
-              );
-            })}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          onPageChange={this.handlePageChange}
-          currentPage={currentPage}
-          pageSize={pageSize}
-        />
-      </React.Fragment>
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={genres}
+            onSelected={this.handleGenereSelect}
+            selectedGenere={selectedGenere}
+          />
+        </div>
+        <div className="col">
+          <h2>This is movies</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th />
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map(movie => {
+                return (
+                  <MovieRow
+                    key={movie._id}
+                    movie={movie}
+                    onDeletePress={this.onDeletePress}
+                    onLikeClick={this.onLikePress}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+          <Pagination
+            itemsCount={count}
+            onPageChange={this.handlePageChange}
+            currentPage={currentPage}
+            pageSize={pageSize}
+          />
+        </div>
+      </div>
     );
   }
 }
---
+++ src/services/fakeGenreService.js
@@ -0,0 +1,7 @@
+export function getGenere() {
+  return [
+    { _id: 1, name: "Action" },
+    { _id: 2, name: "Comedy" },
+    { _id: 3, name: "Thriller" }
+  ];
+}
