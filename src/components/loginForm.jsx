1fd4e7ba955ac6d9c493fa00792ea7266e74a7d2
jyzHome <jyzgo@163.com>
2019-02-10 23:00:01 +1100

    add error when user and password is empty

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -15,7 +15,7 @@ class LoginForm extends Component {
       errors.password = "Password is required";
     }
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    return Object.keys(errors).length === 0 ? {} : errors;
   };
   handleSubmit = e => {
     e.preventDefault();
@@ -24,13 +24,32 @@ class LoginForm extends Component {
     if (errors) return;
     //Call the server
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") {
+        return "Username is required";
+      }
+    }
+
+    if (name === "password" && value.trim() === "") {
+      return "Password is required";
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) {
+      errors[input.name] = errorMessage;
+    } else {
+      delete errors[input.name];
+    }
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
-    const account = this.state.account;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -39,12 +58,14 @@ class LoginForm extends Component {
             name="username"
             value={account.username}
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <Input
             name="password"
             value={account.password}
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
