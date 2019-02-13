4bccd561edec77e10da20a3471e7ad5730566438
jyzHome <jyzgo@163.com>
2019-02-13 15:42:44 +1100

    modify movie form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1>Movie Form {match.params.id} </h1>
-      <button
-        className="btn btn-premary"
-        onClick={() => history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class MovieForm extends Form {
+  state = {
+    data: { title: "", genre: "", numberinstock: "", rate: "" },
+    errors: {}
+  };
+  schema = {
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genre: Joi.string()
+      .required()
+      .label("Genre")
+      .min(5),
+    numberinstock: Joi.number()
+      .required()
+      .min(0)
+      .label("Number in Stock"),
+    rate: Joi.number()
+      .label("Rate")
+      .min(0)
+      .max(5)
+      .required()
+  };
+
+  render() {
+    const { match, history } = this.props;
+    return (
+      <div>
+        <h1>Movie Form </h1>
+        {this.renderInput("title", "Title")}
+        {this.renderInput("genre", "Genre")}
+        {this.renderInput("numberinstock", "Number in Stock")}
+        {this.renderInput("rate", "Rate")}
+        {this.renderButton("Save")}
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
